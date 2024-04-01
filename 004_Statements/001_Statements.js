0493d4c4f33e65453e81b6f48a3fd241f9077d72
Ryan Docherty <ryandocherty95@gmail.com>
2024-04-01 19:43:42 +0100

    Changes to be committed: modified:   003_Output/000_HTML.html new file:   004_Statements/000_HTML.html deleted:    004_Statements/001_Statements.html new file:   004_Statements/001_Statements.js

---
+++ 004_Statements/001_Statements.js
@@ -0,0 +1,26 @@
+//----------JavaScript Statements----------
+
+//JavaScript statements are composed of:
+//Values, Operators, Expressions, Keywords, and Comments
+
+let x, y, z;  // Statement 1
+x = 5;        // Statement 2
+y = 6;        // Statement 3
+z = x + y;    // Statement 4
+
+function valueOfZ_Function(){
+    document.getElementById("demo").innerHTML =
+            "The sum of " + x + " and " + y + " is " + z + ".";
+}
+
+
+//Most JavaScript programs contain many JavaScript statements
+//In HTML, JavaScript statements are executed by the browser
+//The statements are executed, one by one, in the same order as they are written
+//JavaScript programs (and JavaScript statements) are often called JavaScript code
+
+//This statement tells the browser to write "Hello Dolly." inside an HTML element with id="demo":
+
+function writeHelloDolly_Function(){
+    document.getElementById("demo1").innerHTML = "Paragraph Changed";
+}
