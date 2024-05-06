92ce3b3e204310c5912cc929a75785e298841a4b
Ryan Docherty <ryandocherty95@gmail.com>
2024-05-06 16:10:17 +0100

    Changes to be committed: new file:   012_Functions/000_HTML.html new file:   012_Functions/001_Functions.js

---
+++ 012_Functions/001_Functions.js
@@ -0,0 +1,25 @@
+//----------JavaScript Functions----------
+
+
+
+//A JavaScript function is a block of code designed to perform a particular task.
+//A JavaScript function is executed when "something" invokes it (calls it).
+
+
+//Function to compute the product of p1 and p2:
+
+function multiplyTwoNumbers_Function(p1, p2){
+    return p1 * p2;
+}
+
+
+let result = multiplyTwoNumbers_Function(4, 3);
+
+
+function printResult_Function(){
+
+  document.getElementById("myPrintResult").innerHTML =
+  'function multiplyTwoNumbers_Function(p1, p2) { return p1 * p2; }' + '<br>' + '<br>' +
+  'let result = multiplyTwoNumbers_Function(4, 3);' + '<br>' + '<br>' +
+  'result = ' + result;
+}
