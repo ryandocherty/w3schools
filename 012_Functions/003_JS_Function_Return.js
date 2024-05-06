35d2cff6e82b91d7c447b0cc20ede7db78bd27ff
Ryan Docherty <ryandocherty95@gmail.com>
2024-05-06 19:05:48 +0100

    Changes to be committed: modified:   012_Functions/000_HTML.html renamed:    012_Functions/001_Functions.js -> 012_Functions/001_JS_Functions.js new file:   012_Functions/002_JS_Function_Syntax.js new file:   012_Functions/003_JS_Function_Return.js

---
+++ 012_Functions/003_JS_Function_Return.js
@@ -0,0 +1,7 @@
+//----------Function Return----------
+
+
+
+//When JavaScript reaches a return statement, the function will stop executing.
+//If the function was invoked from a statement, JavaScript will "return" to execute the code after the invoking statement.
+//Functions often compute a return value. The return value is "returned" back to the "caller":
